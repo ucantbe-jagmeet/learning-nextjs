d56ba3822ffa8ad573a4b9327309848776d019e7
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-05 23:23:54 +0530

    fetching data from local

--- lama-dev/src/app/portfolio/[category]/page.js
+++ lama-dev/src/app/portfolio/[category]/page.js
@@ -2,27 +2,43 @@ import React from "react";
 import styles from "./page.module.css";
 import Button from "@/components/Button/Button";
 import Image from "next/image";
+import { items } from "./data";
+import { notFound } from "next/navigation";
+
+const getData = (categ) => {
+  const data = items[categ];
+  if (data) {
+    return data;
+  }
+  return notFound();
+};
 
 const Category = ({ params }) => {
+  const data = getData(params.category);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
 
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.title}>Description</p>
-          <Button text="See More" url="#" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-            fill={true}
-            className={styles.img}
-          />
-        </div>
-      </div>
+      {data.map((item) => {
+        return (
+          <div className={styles.item} key={item.id}>
+            <div className={styles.content}>
+              <h1 className={styles.title}>{item.title}</h1>
+              <p className={styles.desc}>{item.desc}</p>
+              <Button text="See More" url="#" />
+            </div>
+            <div className={styles.imgContainer}>
+              <Image
+                src={item.image}
+                alt=""
+                fill={true}
+                className={styles.img}
+              />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
