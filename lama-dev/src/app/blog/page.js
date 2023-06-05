2773e78a842e9f1e57e2b558fb8987090005c1dd
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-05 06:35:05 +0530

    next js data fetching

--- lama-dev/src/app/blog/page.js
+++ lama-dev/src/app/blog/page.js
@@ -3,54 +3,44 @@ import styles from "./page.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-const Blog = () => {
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+const Blog = async () => {
+  const data = await getData();
+
+  const newData = data.slice(0, 7);
+
   return (
     <div className={styles.mainContainer}>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.img}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.title}>Description</p>
-        </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.img}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.title}>Description</p>
-        </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.img}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.title}>Description</p>
-        </div>
-      </Link>
+      {newData.map((item) => {
+        const { id, title, body } = item;
+        return (
+          <Link href="/blog/testId" className={styles.container} key={id}>
+            <div className={styles.imgContainer}>
+              <Image
+                src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+                width={400}
+                height={250}
+                className={styles.img}
+              />
+            </div>
+            <div className={styles.content}>
+              <h1 className={styles.title}>{title}</h1>
+              <p className={styles.desc}>{body}</p>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 };
