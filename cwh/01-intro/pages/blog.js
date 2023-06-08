070f9a089a845bceddedc6a96d14a50d3e7900bc
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-08 05:32:34 +0530

    Creating blog and Blog Post

--- cwh/01-intro/pages/blog.js
+++ cwh/01-intro/pages/blog.js
@@ -1,7 +1,36 @@
 import React from "react";
+import styles from "../styles/Blog.module.css";
+import Link from "next/link";
 
 const Blog = () => {
-  return <div>Blog Page</div>;
+  return (
+    <main className={styles.mainBlog}>
+      <div>
+        <Link href="/blogpost/learn-javascript">
+          <h3 className={styles.blogItemh3}>How to learn javascript in 2022</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+            nisi numquam omnis eaque quam libero quae laudantium illum dicta
+            dolor?
+          </p>
+        </Link>
+      </div>
+      <div>
+        <h3 className={styles.blogItemh3}>How to learn javascript in 2022</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, nisi
+          numquam omnis eaque quam libero quae laudantium illum dicta dolor?
+        </p>
+      </div>
+      <div>
+        <h3 className={styles.blogItemh3}>How to learn javascript in 2022</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, nisi
+          numquam omnis eaque quam libero quae laudantium illum dicta dolor?
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default Blog;
--- cwh/01-intro/pages/blogpost/[slug].js
+++ cwh/01-intro/pages/blogpost/[slug].js
@@ -1,10 +1,22 @@
 import React from "react";
 import { useRouter } from "next/router";
-
+import styles from "../../styles/Blog.module.css";
 const Slug = () => {
   const router = useRouter();
   const { slug } = router.query;
-  return <div>{slug}</div>;
+  return (
+    <main className={styles.mainBlog}>
+      <div className={styles.blogPostItem}>
+        <h1>Title of the page {slug}</h1>
+        <hr />
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+          magni, maiores voluptatem voluptate hic earum voluptatibus ab officiis
+          ducimus iste veniam quaerat modi.
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Slug;
