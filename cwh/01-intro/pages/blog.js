8d8ed9d1a60110d26a00df350cdeea1710c515b0
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-10 05:54:07 +0530

    changes in blog layout

--- cwh/01-intro/pages/blog.js
+++ cwh/01-intro/pages/blog.js
@@ -1,44 +1,47 @@
-import React, { useEffect, useState } from "react";
-import styles from "../styles/Blog.module.css";
-import Link from "next/link";
-import * as fs from "fs";
+import React, { useEffect, useState } from 'react';
+import styles from '../styles/Blog.module.css';
+import Link from 'next/link';
+import * as fs from 'fs';
 
 const Blog = (props) => {
-  const [blogs, setBlogs] = useState(props.allBlogs);
-  // useEffect(() => {
+	const [ blogs, setBlogs ] = useState(props.allBlogs);
+	// useEffect(() => {
 
-  // }, []);
+	// }, []);
 
-  return (
-    <main className={styles.mainBlog}>
-      <div className={styles.blogPostItem}>
-        {blogs.map((blog, index) => {
-          const { title, metadesc, author, slug } = blog;
+	return (
+		<main className={styles.mainBlog}>
+			<div className={styles.blogPostItem}>
+				{blogs.map((blog, index) => {
+					const { title, metadesc, author, slug } = blog;
 
-          return (
-            <Link href={`/blogpost/${slug}`} key={index}>
-              <h3 className={styles.blogItemh3}>{title}</h3>
-              <p>{metadesc.slice(0, 130)}...</p>
-            </Link>
-          );
-        })}
-      </div>
-    </main>
-  );
+					return (
+						<Link href={`/blogpost/${slug}`} key={index}>
+							<h3 className={styles.blogItemh3}>{title}</h3>
+							<p>{metadesc.slice(0, 130)}...</p>
+							<Link href={`/blogpost/${slug}`}>
+								<button className={styles.btn}>Read More</button>
+							</Link>
+						</Link>
+					);
+				})}
+			</div>
+		</main>
+	);
 };
 
 export async function getStaticProps() {
-  let data = await fs.promises.readdir("blogData");
-  let myFile;
-  let allBlogs = [];
-  for (let i = 0; i < data.length; i++) {
-    const item = data[i];
-    myFile = await fs.promises.readFile("blogdata/" + item, "utf-8");
-    allBlogs.push(JSON.parse(myFile));
-  }
-  return {
-    props: { allBlogs },
-  };
+	let data = await fs.promises.readdir('blogData');
+	let myFile;
+	let allBlogs = [];
+	for (let i = 0; i < data.length; i++) {
+		const item = data[i];
+		myFile = await fs.promises.readFile('blogdata/' + item, 'utf-8');
+		allBlogs.push(JSON.parse(myFile));
+	}
+	return {
+		props: { allBlogs }
+	};
 }
 
 export default Blog;
