11b6f42b34f992aeb0ae919c84caa5bb8041408b
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-13 00:33:21 +0530

    Changes in blogdata Slug

--- cwh/01-intro/pages/blogpost/[slug].js
+++ cwh/01-intro/pages/blogpost/[slug].js
@@ -19,12 +19,12 @@ const Slug = (props) => {
 };
 
 export async function getStaticPaths() {
+  let allb = await fs.promises.readdir(`blogData`);
+  allb = allb.map((item)=>{
+    return   { params: { slug:item.split(".")[0]}}
+  })
   return {
-    paths: [
-      { params: { slug: "how-to-learn-flask" } },
-      { params: { slug: "how-to-learn-javascript" } },
-      { params: { slug: "how-to-learn-nextjs" } },
-    ],
+    paths: allb,
     fallback: true,
   };
 }
