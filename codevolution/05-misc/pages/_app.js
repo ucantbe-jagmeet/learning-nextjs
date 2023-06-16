abdb5005c2062eba1527790ba16b6f319b383039
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-16 09:19:49 +0530

    App Layout

---
+++ codevolution/05-misc/pages/_app.js
@@ -0,0 +1,17 @@
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import '../styles/globals.css'
+import '../styles/layout.css'
+
+function MyApp({ Component, pageProps }) {
+  // return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
+}
+
+export default MyApp
