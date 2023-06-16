f82723c540d768f8b3ac7e003d21fc6b92fce4e6
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-16 09:24:12 +0530

    Head Component

--- codevolution/05-misc/pages/_app.js
+++ codevolution/05-misc/pages/_app.js
@@ -1,17 +1,19 @@
+import Head from 'next/head'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import '../styles/globals.css'
 import '../styles/layout.css'
-
 function MyApp({ Component, pageProps }) {
-  // return <Component {...pageProps} />
   return (
     <>
+      <Head>
+        <title>Codevolution</title>
+        <meta name='description' content='Awesome YouTube channel' />
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
     </>
   )
 }
-
 export default MyApp
--- codevolution/05-misc/pages/about.js
+++ codevolution/05-misc/pages/about.js
@@ -1,5 +1,16 @@
+import Head from 'next/head'
+
 function About() {
-    return <h1 className='content'>About</h1>
-  }
-  
-  export default About
+//   return <h1 className='content'>About</h1>
+  return (
+    <>
+      <Head>
+        <title>About Jagmeet</title>
+        <meta name='description' content='Free tutorials on web development' />
+      </Head>
+      <h1 className='content'>About</h1>
+    </>
+  )
+}
+
+export default About
