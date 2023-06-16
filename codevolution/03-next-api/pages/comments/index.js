fa75bcf004154a3ffadfc58c657ac61f954b9f4b
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-16 07:35:12 +0530

    api post request

--- codevolution/03-next-api/pages/comments/index.js
+++ codevolution/03-next-api/pages/comments/index.js
@@ -3,14 +3,25 @@ import { useState } from "react"
 function CommentsPage(){
 
     const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState('');
     const fetchComments = async()=>{
         const resp = await fetch('/api/comments')
         const data = await resp.json()
         setComments(data)
     }
+    const submitComment = async () =>{
+        const resp = await fetch('/api/comments', { method: 'POST' , body: JSON.stringify({comment}), headers:{
+            'Content-Type':'application/json'
+        }  })
+        const data = await resp.json()
+        console.log(data);
+    }
 
 
     return<>
+        <input type="text" value={comment} onChange={e => setComment(e.target.value)} />
+        <button onClick={submitComment}>Submit commit</button>
+
         <button onClick={fetchComments}>Load Comments</button>
         <p>{comments.map(comment=>{
             return <div key ={comment.id}>
