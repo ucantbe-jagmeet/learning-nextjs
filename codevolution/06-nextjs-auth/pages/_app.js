3cd79259e4961598b5d2437a1d996b50c84b5438
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-17 01:40:06 +0530

    NextAuth Provider

--- codevolution/06-nextjs-auth/pages/_app.js
+++ codevolution/06-nextjs-auth/pages/_app.js
@@ -1,13 +1,14 @@
 import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 import '../components/Navbar.css'
+import { Provider } from 'next-auth/client'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Provider>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </Provider>
   )
 }
 
--- codevolution/06-nextjs-auth/pages/index.js
+++ codevolution/06-nextjs-auth/pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useSession } from 'next-auth/client'
 
 export default function Home() {
+
+  const[session , loading] = useSession()
+console.log(session);
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +17,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+           {session ? `${session.user.name}, `: ""}  Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
