9f56dca5e97523ab1b4f4fe14f92d281cc917a2e
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-17 02:03:19 +0530

    Securing pages server side

--- codevolution/06-nextjs-auth/pages/blog.js
+++ codevolution/06-nextjs-auth/pages/blog.js
@@ -9,6 +9,16 @@ export default Blog
 
 export async function getServerSideProps(context){
     const session = await getSession(context)
+
+    if(!session){
+        return{
+            redirect:{
+                destination: `/api/auth/signin?callbackUrl=http://localhost:3000/blog`,
+                permanent: false
+            }
+        }
+    }
+
     return {
         props:{
             session,
