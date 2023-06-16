f4666bea80c72170e6369bec38a9244e4e2c721b
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-17 01:07:17 +0530

    Client side Authentication

--- codevolution/06-nextjs-auth/components/Navbar.js
+++ codevolution/06-nextjs-auth/components/Navbar.js
@@ -1,14 +1,15 @@
 import Link from 'next/link'
-import { signIn, signOut} from "next-auth/client"
+import { signIn, signOut, useSession} from "next-auth/client"
 
 
 function Navbar() {
+    const[ session , loading] = useSession()
   return (
     <nav className='header'>
       <h1 className='logo'>
         <a href='#'>NextAuth</a>
       </h1>
-      <ul className='main-nav'>
+      <ul className={`main-nav ${!session && loading ? 'loading':'loaded'}`}>
       <li>
           <Link href='/' legacyBehavior>
             <a>Home</a>
@@ -25,22 +26,34 @@ function Navbar() {
           </Link>
         </li>
 
-        <li>
-            <Link href='/api/auth/signin' legacyBehavior>  
-              <a onClick={e=>{
-                e.preventDefault()
-                signIn('github')
-              }}>Sign In</a>
-            </Link>
-        </li>
-        <li>
-            <Link href='/api/auth/signout' legacyBehavior>  
-            <a onClick={e=>{
-                e.preventDefault()
-                signOut()
-              }}>Sign Out</a>
-            </Link>
-        </li>
+        {
+            !loading && !session && (
+                <li>
+                    <Link href='/api/auth/signin' legacyBehavior>  
+                    <a onClick={e=>{
+                        e.preventDefault()
+                        signIn('github')
+                        }}>Sign In</a>
+                    </Link>
+                </li>
+            )
+        }
+
+
+        {
+            session && (
+                 <li>
+                    <Link href='/api/auth/signout' legacyBehavior>  
+                        <a onClick={e=>{
+                        e.preventDefault()
+                        signOut()
+                        }}>
+                            Sign Out
+                        </a>
+                </Link>
+                </li>)
+        }
+       
         
       </ul>
     </nav>
