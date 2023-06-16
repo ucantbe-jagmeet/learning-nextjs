4529b4bd0800118436ab42d0a3b4aee985a15ddb
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-16 08:00:31 +0530

    api delete request

--- codevolution/03-next-api/pages/comments/index.js
+++ codevolution/03-next-api/pages/comments/index.js
@@ -17,6 +17,14 @@ function CommentsPage(){
         console.log(data);
     }
 
+    const deleteComment = async( commentId)=>{
+        const response = await fetch(`/api/comments/${commentId}`, {
+            method:'DELETE',
+        })
+        const data = await response.json()
+        console.log(data);
+        fetchComments()
+    }
 
     return<>
         <input type="text" value={comment} onChange={e => setComment(e.target.value)} />
@@ -26,6 +34,7 @@ function CommentsPage(){
         <p>{comments.map(comment=>{
             return <div key ={comment.id}>
                 {comment.id} {comment.text}
+                <button onClick={()=>  deleteComment(comment.id)}>Delete</button>
             </div>
         })}</p>
     </>
