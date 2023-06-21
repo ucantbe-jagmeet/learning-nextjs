d822d4082fa6b24d8cc1f17988683b101ca5c5d0
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-21 11:52:11 +0530

    Login and signup password encrypted

--- cwh/02-codeswear/pages/api/login.js
+++ cwh/02-codeswear/pages/api/login.js
@@ -1,14 +1,18 @@
 import connectDb from "../../middleware/mongoose"
 import User from "../../models/User"
+var CryptoJS = require('crypto-js')
 
 const handler = async ( req, res) => {
 
     if(req.method == 'POST'){
 
         let user = await User.findOne({ "email": req.body.email})
+        var bytes  = CryptoJS.AES.decrypt(user.password, process.env.CRYPTO_SECRET_KEY);
+        var decryptedPass = bytes.toString(CryptoJS.enc.Utf8)
+        console.log(decryptedPass);
         if(user){
-            if( req.body.email == user.email && req.body.password == user.password){
-                res.status(200).json({msg:"success", name: user.name,email:user.email})
+            if( (req.body.email == user.email )&& ( req.body.password == decryptedPass) ){
+                res.status(200).json({msg:"success", name: user.name, email:user.email})
             }
            else{
             res.status(400).json({msg:"not success", error : "Invalid Credentials"})
