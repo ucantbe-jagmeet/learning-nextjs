a2bbf5c4c6b33d3836c01c6f910bc6762512ce9a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-07 16:45:42 +0530

    connecting mongodb

---
+++ cwh/03-stock-management/app/api/mongo/route.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+import { NextResponse } from "next/server";
+
+export async function GET(request){
+    
+    const uri = process.env.MONGO_URI;
+    const client = new MongoClient(uri)
+        try {
+            const database = client.db('03-stock-management');
+            const movies = database.collection('inventory');
+            
+            const query = {}
+            const movie = await movies.findOne(query);
+            console.log(movie);
+            return NextResponse.json({ "a":34, movie})
+        } finally  {
+            await client.close()
+        }
+
+}
