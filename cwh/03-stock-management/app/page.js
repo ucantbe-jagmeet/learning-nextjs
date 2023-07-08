3c42d9bae82207235755762e210004a8f1669a3c
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-08 22:13:57 +0530

    dropdown for search button

--- cwh/03-stock-management/app/page.js
+++ cwh/03-stock-management/app/page.js
@@ -7,6 +7,22 @@ export default function Home() {
   const [productForm, setProductForm] = useState({});
   const [products, setProducts] = useState([]);
   const [alert, setAlert] = useState('');
+  const [dropdown, setDropdown] = useState([{
+    _id: "64a96d89677819981f76d79e",
+    slug: "jordam",
+    quantity: "11",
+    price: "1100"
+  },
+  {
+    _id: "64a96c98677819981f76d79c",
+    slug: "shirt",
+    quantity: "19",
+    price: "299"
+  }
+]);
+
+const [loading, setLoading] = useState(false);
+const [query, setQuery] = useState("");
   
   
   const addProduct = async (e)=>{
@@ -47,7 +63,18 @@ export default function Home() {
     fetchProducts()
   },[])
   
-  
+    const onDropdownEdit = async(e) => {
+        setQuery(e.target.value)
+        if(!loading){
+          setLoading(true)
+            const response = await fetch(`/api/search?query=${query}`)
+            let rjson = await response.json()
+            setDropdown(rjson.products)
+          setLoading(false)
+        }
+    }
+    
+
   return (
     <>
            <Header />
@@ -55,7 +82,7 @@ export default function Home() {
         <div className='text-green-800 text-center'>{alert}</div>
         <h1 className="text-3xl font-semibold mb-6">Search a Product</h1>
         <div className="flex mb-2">
-          <input  type="text" placeholder="Enter a product name" className="flex-1 border border-gray-300 px-4 py-2 rounded-l-md" />
+          <input onChange={onDropdownEdit} type="text" placeholder="Enter a product name" className="flex-1 border border-gray-300 px-4 py-2 rounded-l-md" />
           <select className="border border-gray-300 px-4 py-2 rounded-r-md">
             <option value="">All</option>
             <option value="category1">Category 1</option>
@@ -63,23 +90,28 @@ export default function Home() {
             {/* Add more options as needed */}
           </select>
         </div>
-        {/* {loading && <div className='flex justify-center items-center'> <img width={74} src="/loading.svg" alt="" /> </div>
-        } */}
+        {loading && <div className='flex justify-center items-center'> <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100">
+          <circle cx="50" cy="50" r="40" stroke="blue" stroke-width="3" fill="none">
+            <animateTransform attributeName="transform" attributeType="XML" type="rotate" from="0 50 50" to="360 50 50" dur="2s" repeatCount="indefinite" />
+          </circle>
+        </svg>
+          </div>
+        }
         <div className="dropcontainer absolute w-[72vw] border-1 bg-purple-100 rounded-md ">
-          {/* 
+          
           {dropdown.map(item => {
             return <div key={item.slug} className="container flex justify-between p-2 my-1 border-b-2">
 
               <span className="slug"> {item.slug} ({item.quantity} available for ₹{item.price})</span>
               <div className='mx-5'>
-                <button onClick={() => { buttonAction("minus", item.slug, item.quantity) }} disabled={loadingaction} className="subtract inline-block px-3 py-1 cursor-pointer bg-purple-500 text-white font-semibold rounded-lg shadow-md disabled:bg-purple-200"> - </button>
+                <button onClick={() => { buttonAction("minus", item.slug, item.quantity) }}  className="subtract inline-block px-3 py-1 cursor-pointer bg-purple-500 text-white font-semibold rounded-lg shadow-md disabled:bg-purple-200"> - </button>
 
                 <span className="quantity inline-block  min-w-3 mx-3">{item.quantity}</span>
-                <button onClick={() => { buttonAction("plus", item.slug, item.quantity) }} disabled={loadingaction} className="add inline-block px-3 py-1 cursor-pointer bg-purple-500 text-white font-semibold rounded-lg shadow-md disabled:bg-purple-200">  + </button>
+                <button onClick={() => { buttonAction("plus", item.slug, item.quantity) }}  className="add inline-block px-3 py-1 cursor-pointer bg-purple-500 text-white font-semibold rounded-lg shadow-md disabled:bg-purple-200">  + </button>
 
               </div>
             </div>
-          })} */}
+          })}
         </div>
       </div>
 
