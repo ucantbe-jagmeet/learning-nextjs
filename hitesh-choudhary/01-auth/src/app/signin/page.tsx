b3d6c6591e75484771c09522f5b70d3fb6f2fa6b
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-08-10 15:44:18 +0530

    api routes setup

--- hitesh-choudhary/01-auth/src/app/signin/page.tsx
+++ hitesh-choudhary/01-auth/src/app/signin/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div className="bg-white text-black">Login Page</div>;
+  return <div className="bg-white text-black">Sign in Page</div>;
 };
 
 export default page;
