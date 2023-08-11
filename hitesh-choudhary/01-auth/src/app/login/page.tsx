5202195fbb6c7611102f1fb5ff2789cca26fb5d5
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-08-11 20:05:52 +0530

    login page

--- hitesh-choudhary/01-auth/src/app/login/page.tsx
+++ hitesh-choudhary/01-auth/src/app/login/page.tsx
@@ -8,16 +8,38 @@ import axios from "axios";
 import { toast } from "react-hot-toast";
 
 const page = () => {
+  const router = useRouter();
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const onlogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login success", response.data);
+      toast.success("Login success");
+      router.push("/profile");
+      setLoading(false);
+    } catch (error: any) {
+      console.log("Login failed", error.message);
+      toast.error(error.message);
+    }
+  };
 
-  const onlogin = async () => {};
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h2>login</h2>
+      <h1>{loading ? "Processing" : "Login"}</h1>
       <hr />
 
       <label htmlFor="email">email</label>
@@ -28,7 +50,7 @@ const page = () => {
         value={user.email}
         onChange={(e) => setUser({ ...user, email: e.target.value })}
         placeholder="email"
-        className="p-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:border-gray-600 text-black"
       />
       <label htmlFor="password">password</label>
       <input
@@ -38,7 +60,7 @@ const page = () => {
         value={user.password}
         onChange={(e) => setUser({ ...user, password: e.target.value })}
         placeholder="password"
-        className="p-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:border-gray-600 text-black"
       />
       <button
         onClick={onlogin}
