3376bfbc2b3694a28d8217dd04db81757013a9ba
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-08-14 10:24:19 +0530

    getting user details from helper function

--- hitesh-choudhary/01-auth/src/app/profile/page.tsx
+++ hitesh-choudhary/01-auth/src/app/profile/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 /* eslint-disable react-hooks/rules-of-hooks */
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import Link from "next/link";
 import { toast } from "react-hot-toast";
@@ -8,6 +8,8 @@ import { useRouter } from "next/navigation";
 
 const page = () => {
   const router = useRouter();
+  const [data, setData] = useState("nothing");
+  console.log(data);
 
   const logout = async () => {
     try {
@@ -20,12 +22,31 @@ const page = () => {
     }
   };
 
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <h1>Profile Page</h1>
+      <h2 className="p-1 bg-green-500 rounded">
+        {data === "nothing" ? (
+          "nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
       <hr />
+      <button
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-10"
+        onClick={getUserDetails}
+      >
+        User Details
+      </button>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-10"
         onClick={logout}
