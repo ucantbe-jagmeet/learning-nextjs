439d2e343d03ef46c2d2b4f9e4241888ad133029
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-08-14 09:32:33 +0530

    logout functionality done on profile page

--- hitesh-choudhary/01-auth/src/app/profile/page.tsx
+++ hitesh-choudhary/01-auth/src/app/profile/page.tsx
@@ -1,11 +1,37 @@
+"use client";
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from "react";
+import axios from "axios";
+import Link from "next/link";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 const page = () => {
+  const router = useRouter();
+
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successfull");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <h1>Profile Page</h1>
+      <hr />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-10"
+        onClick={logout}
+      >
+        Logout
+      </button>
     </div>
   );
 };
