d654588c60a50ff4d8777802b5f44b0f5e980312
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-08-15 20:25:00 +0530

    nodemailer initial setup

---
+++ hitesh-choudhary/01-auth/src/helpers/mailer.ts
@@ -0,0 +1,24 @@
+import User from "@/models/userModel";
+import nodemailer from "nodemailer";
+import bcryptjs from "bcryptjs";
+
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+  try {
+    // create a hased token
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
