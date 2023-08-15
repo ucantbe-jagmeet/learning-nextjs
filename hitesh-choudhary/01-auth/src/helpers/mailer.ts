4322d74306d13f4476a6e9579e356bd1033f99ab
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-08-15 20:30:11 +0530

    nodemailer completed

--- hitesh-choudhary/01-auth/src/helpers/mailer.ts
+++ hitesh-choudhary/01-auth/src/helpers/mailer.ts
@@ -28,6 +28,22 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
       },
     });
 
+    const mailOptions = {
+      from: "jagmeetsingh29213@gmail.com",
+      to: email,
+      subject:
+        emailType === "VERIFY" ? "Verify your email" : "Reset your password",
+      html: `<p>Click <a href="${
+        process.env.DOMAIN
+      }/verifyemail?token=${hashedToken}">here</a> to ${
+        emailType === "VERIFY" ? "verify your email" : "reset your password"
+      }
+            or copy and paste the link below in your browser. <br> ${
+              process.env.DOMAIN
+            }/verifyemail?token=${hashedToken}
+            </p>`,
+    };
+
     const mailresponse = await transport.sendMail(mailOptions);
     return mailresponse;
   } catch (error: any) {
